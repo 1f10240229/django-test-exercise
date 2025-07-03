fccbbd5c03a93930a405f730b31d4093f2835518
1F10240229 <s1f102402293@iniad.org>
2025-07-03 11:15:02 +0900

    細かい修正3

--- todo/tests.py
+++ todo/tests.py
@@ -8,6 +8,7 @@ class SampleTestCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1 + 2, 3)
 
+class TaskModelTestCase(TestCase):
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
         task = Task(title='task1', due_at=due)
@@ -31,9 +32,27 @@ class SampleTestCase(TestCase):
         due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
         current = timezone.make_aware(datetime(2024, 6, 30, 0, 0, 0))
         task = Task(title='task1', due_at = due)
-
+        task.save()
+        
         self.assertFalse(task.is_overdue(current))
     
+    def test_i_overdue_past(self):
+        due = timezone.make_aware(datetime(2024, 6, 30, 23, 59, 59))
+        current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
+        task = Task(title='task1', due_at = due)
+        task.save()
+
+        self.assertTrue(task.is_overdue(current))
+
+    
+    def test_i_overdue_none(self):
+        due = None
+        current = timezone.make_aware(datetime(2024, 7, 1, 0, 0, 0))
+        task = Task(title='task1', due_at = due)
+        task.save()
+
+        self.assertFalse(task.is_overdue(current))
+
 class TodoViewTestCase(TestCase):
     def test_index_get(self):
         client = Client()
